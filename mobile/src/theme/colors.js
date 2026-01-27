0036a3951c7e490fef1781ded3dd014c359752d2
OOLAHIANE MOHAMED <oolahianemohamed1@gmail.com>
2026-01-27 01:36:11 +0100

    add primary light color to theme/colors

--- mobile/src/theme/colors.js
+++ mobile/src/theme/colors.js
@@ -1,5 +1,7 @@
 export const colors = {
   primary: '#ce4218',
+  success: '#33be17',
+  primaryLight: '#dddddd',
   backgroundLight: '#f6f6f8',
   backgroundDark: '#111621',
   textDark: '#111318',
